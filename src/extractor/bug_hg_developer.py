f2c3a7a329a81b29031c22d35db019d234e929fd
Akond Rahman <akond.rahman.buet@gmail.com>
2016-10-05 11:04:20 -0400

    check if variable is integer

--- src/extractor/bug_hg_developer.py
+++ src/extractor/bug_hg_developer.py
@@ -45,7 +45,8 @@ def getChurnFromStr(splitted_param):
       elem=elem.split('\n')[0]
     else:
       elem = elem 
-    if (elem !='') and (isinstance(elem, int)):      
+    if (elem !=''):      
+    #if (elem !='') and (isinstance(elem, int)):      
       tmp_ = int(elem) 
     valToRet = valToRet + tmp_          	
   return valToRet	
