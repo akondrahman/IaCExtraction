32f866bfde7e80ddd7edf956c5f588a1645e29e2
Akond Rahman <akond.rahman.buet@gmail.com>
2016-10-07 12:10:53 -0400

    modeling with log transform

--- src/modeling/phase2_main.py
+++ src/modeling/phase2_main.py
@@ -16,3 +16,9 @@ Deprecating warnings will be suppressed
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 dataset_file="/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Dataset/LOCKED_WIKIMEDIA_23_REPOS_DATASET.csv"
 full_dataset_from_csv = Utility.getDatasetFromCSV(dataset_file)
+full_rows, full_cols = np.shape(full_dataset_from_csv)
+## we will skip the first column, as it has file names 
+feature_cols = full_cols - 2  ## the last couln is null, and have to skip bug count, so two colums  to skip 
+all_features = full_dataset_from_csv[:, 1:feature_cols]
+print "Glimpse at features (10th entry in dataset): \n", all_features[9]
+print "-"*50
