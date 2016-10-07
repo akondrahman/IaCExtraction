f0f95fa4311c4dca82926aa83c85c4bdc80c97f5
Akond Rahman <akond.rahman.buet@gmail.com>
2016-10-06 22:02:12 -0400

    saving

--- src/modeling/phase1_main.py
+++ src/modeling/phase1_main.py
@@ -46,11 +46,20 @@ print "-"*50
     10 fold validation instaed of bootstrap 
 '''  
 fold2Use =10 
+'''
+Single iteration zone : turn off 'performIterativeModeling()'
+while running this 
+'''
 # this method runs the classifiers once
-#sklearn_models.performModeling(selected_features, formatted_labels, fold2Use)
+sklearn_models.performModeling(selected_features, formatted_labels, fold2Use)
 print "-"*50
+'''
+Multiple iteration zone : turn off 'performModeling()'
+while running this 
+'''
 # this method runs the classifiers 'iteration' number of times 
-iteration=1000
-sklearn_models.performIterativeModeling(selected_features, formatted_labels, fold2Use, iteration)
-print "-"*50
+#iteration=1000
+#sklearn_models.performIterativeModeling(selected_features, formatted_labels, fold2Use, iteration)
+#print "-"*50
+
 print "Ended at:", Utility.giveTimeStamp()
