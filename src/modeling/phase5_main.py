ef2639fe7205b735e8c860b0afc00b3203e320a4
Akond Rahman <akond.rahman.buet@gmail.com>
2016-10-19 11:41:39 -0400

    saving-2

--- src/modeling/phase5_main.py
+++ src/modeling/phase5_main.py
@@ -17,9 +17,15 @@ dataset_file="/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Dataset/w
 full_dataset_from_csv = Utility.getDatasetFromCSV(dataset_file)
 full_rows, full_cols = np.shape(full_dataset_from_csv)
 ## we will skip the first column, as it has file names
+feature_cols = full_cols - 3  ## the last couln is null, and have to skip bug count, so two colums  to skip
 all_features = full_dataset_from_csv[:, 1:feature_cols]
-feature_cols = full_cols - 2  ## the last couln is null, and have to skip bug count, so two colums  to skip
-
 print "Glimpse at features (11th entry in dataset): \n", all_features[glimpseIndex]
 print "-"*50
+dataset_for_labels = Utility.getDatasetFromCSV(dataset_file)  ## unlike phase-1, the labels are '1' and '0', so need to take input as str
+label_cols = full_cols - 2
+all_labels  =  dataset_for_labels[:, label_cols]
+print "Glimpse at  labels (11th entry in dataset):", all_labels[glimpseIndex]
+print "-"*50
+
+
 print "Ended at:", Utility.giveTimeStamp()
