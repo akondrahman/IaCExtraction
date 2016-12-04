3ce52302b849c6d7c39c1d362ce620deffb770e2
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-04 02:03:54 -0500

    feature selection experiment

--- src/modeling/exp_feature_selection.R
+++ src/modeling/exp_feature_selection.R
@@ -8,15 +8,31 @@ iac_file_name <- "/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Datas
 iac_pred_data <- read.csv(iac_file_name, header=TRUE, stringsAsFactors=F)
 # print("Before filtering ...")
 # print(summary(iac_pred_data))
+# convertinf defect status to string 
+defect_status <- iac_pred_data$defectStatus
+defect_status <- as.character(defect_status) 
 ## Need to remove the file names by deleting the first column 
-iac_pred_data <- iac_pred_data[, -c(1, 64)]
+iac_pred_data_filtered <- iac_pred_data[, -c(1, 63, 64)]
+# merging the 'defect status' as string vector 
+iac_full_dataframe_ <- data.frame(iac_pred_data_filtered, defect_status)
 print("After filtering ...")
-print(summary(iac_pred_data))
-control_ <- trainControl(method="repeatedcv", number=10, repeats=5)
-# train the model
-#model <- train(defectStatus ~., data=iac_pred_data, method="lvq", preProcess="scale", trControl=control_)
+print(summary(iac_full_dataframe_))
+varImpFlag=TRUE 
+if(varImpFlag)
+{
+  control_ <- trainControl(method="repeatedcv", number=10, repeats=5)
+  # train the model
+  theModel <- train(defect_status ~., data=iac_full_dataframe_, method="lvq", preProcess="scale", trControl=control_)
+  # estimate variable importance
+  importance <- varImp(theModel, scale=FALSE)
+  # summarize importance
+  print(importance)
+  # plot importance
+  plot(importance)  
+}
 
 t2 <- Sys.time()
 time_diff <- t2 - t1 
 print("Time taken ...")
 print(time_diff)
+rm(list = setdiff(ls(), lsf.str()))
