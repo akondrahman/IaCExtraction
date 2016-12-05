4c58d800256bf46e24866b97406293e4e8a3b2fd
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-04 23:49:57 -0500

    saving

--- src/analysis/analysis_categorization.py
+++ src/analysis/analysis_categorization.py
@@ -51,15 +51,16 @@ def loadMessageMapping(dirNameParam):
   for file_ in os.listdir(dirNameParam):
     if file_.endswith(".csv"):
       fullFileName = dirNameParam + file_
+      print "Analyzing for mapping ...", file_
       with open(fullFileName, 'rU') as f:
         reader = csv.reader(f)
-        if len(row) > 0:
-          for row in reader:
-            message_ = row[0]
-            ID_  = row[1]
-            repo_ = getRepoInfo(row[2])
-            if message_ not in messageDict:
-              messageDict[message_] = (ID_, repo_)
+        for row in reader:
+            if len(row) > 0:
+              message_ = row[0]
+              ID_  = row[1]
+              repo_ = getRepoInfo(row[2])
+              if message_ not in messageDict:
+                messageDict[message_] = (ID_, repo_)
   return messageDict
 def summarize(dictParam):
     cat2messageMap=[]
@@ -75,17 +76,34 @@ def summarize(dictParam):
     return cat2messageMap
 
 
+
+def findIDMappingOfStudentMessages(studentDict, idDict):
+   '''
+   This method retruns the ID, and repo for each message categorized by students
+   '''
+   dict2ret={}
+   for k_, v_ in studentDict.iteritems():
+     if k_ in idDict:
+        dict2ret[k_] = idDict[k_]
+   return dict2ret
+
+
 def performInterRaterRelaibility(rating1, rating2):
   from sklearn import metrics
   #ref:: http://scikit-learn.org/stable/modules/generated/sklearn.metrics.cohen_kappa_score.html
   kappa_score = metrics.cohen_kappa_score(rating1, rating2)
   return kappa_score
 dirName='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Categorization/StudentStudy/completed/'
-summary_dict = readFile(dirName)
-cat_messgae_mapping = summarize(summary_dict)
+student_summary_dict = readFile(dirName)
+cat_messgae_mapping = summarize(student_summary_dict)
 #print summary_dict
 #print cat_messgae_mapping
+print "#"*100
 myRatingDir='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Categorization/StudentStudy/completed/'
-mappingDir='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/IaCExtraction/src/analysis/'
+mappingDir='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Categorization/StudentStudy/ided/'
 msgMappingDict = loadMessageMapping(mappingDir)
-print msgMappingDict
+print "Length of unique messages", len(msgMappingDict)
+print "#"*100
+idMappingOfMessages = findIDMappingOfStudentMessages(student_summary_dict, msgMappingDict)
+print "Length of unique student derived mapped messages", len(idMappingOfMessages)
+print "#"*100
