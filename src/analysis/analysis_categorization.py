ba880acc9e649e5d1ea4b75371d400949f77dbd5
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-04 23:29:35 -0500

    saving

--- src/analysis/analysis_categorization.py
+++ src/analysis/analysis_categorization.py
@@ -8,21 +8,59 @@ def readFile(dirNameParam):
   messageDict = {}
   for file_ in os.listdir(dirNameParam):
     if file_.endswith(".csv"):
+      print "Analyzing ...", file_
       fullFileName = dirNameParam + file_
       with open(fullFileName, 'rU') as f:
         reader = csv.reader(f)
         for row in reader:
-           message_ = row[0]
-           catego_  = row[1]
-           if message_ not in messageDict:
-             messageDict[message_] = [catego_]
-           else:
-             existingList = messageDict[message_]
-             tmp_ = existingList + [catego_]
-             messageDict[message_] = tmp_
+           if len(row) > 0:
+             message_ = row[0]
+             catego_  = row[1]
+             if message_ not in messageDict:
+               messageDict[message_] = [catego_]
+             else:
+               existingList = messageDict[message_]
+               tmp_ = existingList + [catego_]
+               messageDict[message_] = tmp_
   return messageDict
 
-
+def getRepoInfo(repoKey):
+    repoPathToRet=''
+    repoDict={
+              'M1':'/Users/akond/PUPP_REPOS/mozilla-releng-downloads/puppet/',
+              'M2':'/Users/akond/PUPP_REPOS/mozilla-releng-downloads/relabs-puppet/',
+              'W1':'/Users/akond/PUPP_REPOS/wikimedia-downloads/cdh/',
+              'W2':'/Users/akond/PUPP_REPOS/wikimedia-downloads/cdh4/',
+              'W3':'/Users/akond/PUPP_REPOS/wikimedia-downloads/kafka',
+              'W4':'/Users/akond/PUPP_REPOS/wikimedia-downloads/kraken/',
+              'W5':'/Users/akond/PUPP_REPOS/wikimedia-downloads/mariadb/',
+              'W6':'/Users/akond/PUPP_REPOS/wikimedia-downloads/mesos/',
+              'W7':'/Users/akond/PUPP_REPOS/wikimedia-downloads/nginx/',
+              'W8':'/Users/akond/PUPP_REPOS/wikimedia-downloads/puppet/',
+              'W9':'/Users/akond/PUPP_REPOS/wikimedia-downloads/translatewiki/',
+              'W10':'/Users/akond/PUPP_REPOS/wikimedia-downloads/vagrant/',
+              'W11':'/Users/akond/PUPP_REPOS/wikimedia-downloads/wikimetrics/',
+              }
+    if repoKey in repoDict:
+        repoPathToRet=repoDict[repoKey]
+    else:
+        repoPathToRet = 'WTF'
+    return repoPathToRet
+def loadMessageMapping(dirNameParam):
+  messageDict = {}
+  for file_ in os.listdir(dirNameParam):
+    if file_.endswith(".csv"):
+      fullFileName = dirNameParam + file_
+      with open(fullFileName, 'rU') as f:
+        reader = csv.reader(f)
+        if len(row) > 0:
+          for row in reader:
+            message_ = row[0]
+            ID_  = row[1]
+            repo_ = getRepoInfo(row[2])
+            if message_ not in messageDict:
+              messageDict[message_] = (ID_, repo_)
+  return messageDict
 def summarize(dictParam):
     cat2messageMap=[]
     for k_, v_ in dictParam.iteritems():
@@ -48,3 +86,6 @@ cat_messgae_mapping = summarize(summary_dict)
 #print summary_dict
 #print cat_messgae_mapping
 myRatingDir='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/Categorization/StudentStudy/completed/'
+mappingDir='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC_Mining/IaCExtraction/src/analysis/'
+msgMappingDict = loadMessageMapping(mappingDir)
+print msgMappingDict
