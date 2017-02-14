4640ae2a702771ed9dd350bc05f696a4f9d344c5
Akond Rahman <akond.rahman.buet@gmail.com>
2017-02-13 20:28:42 -0500

    rq answers

--- src/analysis/ans_rqs.py
+++ src/analysis/ans_rqs.py
@@ -9,6 +9,19 @@ import os, csv, xlrd, collections
 
 notFoundMsg='NotFound'
 WHAT_THE_FUCK='WTF'
+THE_SPACE=' '
+
+
+def dumpContentIntoFile(strP, fileP):
+  fileToWrite = open( fileP, 'w');
+  fileToWrite.write(strP );
+  fileToWrite.close()
+  return str(os.stat(fileP).st_size)
+
+
+
+
+
 def getMappedFile(id_Param, repo_Param):
     fileNameTiRet=notFoundMsg
     createDict={}
@@ -29,6 +42,8 @@ def getMappedFile(id_Param, repo_Param):
           #print "paisi mamare"
           fileNameTiRet=createDict[k_]
     return fileNameTiRet
+
+
 
 
 
@@ -56,7 +71,9 @@ def getMappedTimestamp(id_Param, repo_Param):
     return timestamp2ret
 
 
-def constructDataset(ParamFiles):
+
+def constructDataset(ParamFiles, output_file_param):
+  str2dump=''
   for file_ in ParamFiles:
     #print"file:", file_
     with open(file_, 'rU') as categ_file:
@@ -66,19 +83,27 @@ def constructDataset(ParamFiles):
         id_of_message   = categ_row[0]
         repo_of_message = categ_row[1]
         the_message     = categ_row[2]
+        the_message     = the_message.replace('\n', ' ')
+        the_message     = the_message.replace(',', ';')
         categ_message   = categ_row[3]
         if((repo_of_message!=WHAT_THE_FUCK) and (len(repo_of_message)>0)):
           file_message    = getMappedFile(id_of_message, repo_of_message)
           size_file       = os.stat(file_message).st_size
           lines_for_file  = sum(1 for line in open(file_message))
-          time_message    = getMappedTimestamp(id_of_message, repo_of_message)
+          time_message    = getMappedTimestamp(id_of_message, repo_of_message).split(THE_SPACE)[0]
           print "ID:{}, repo:{}, message:{}, category:{}, file:{}, time:{}, size:{}, lines:{}".format(id_of_message, repo_of_message,
                                                                                                       the_message,   categ_message,
                                                                                                       file_message,  time_message,
                                                                                                       size_file,     lines_for_file)
 
+          str2dump = str2dump + id_of_message + ',' + repo_of_message + ',' + the_message + ',' + categ_message + ',' + file_message + ',' + time_message + ',' + str(size_file) + ',' + str(lines_for_file) + ',' + '\n'
+  stat_ = dumpContentIntoFile(str2dump, output_file_param)
+  print "Dumped a file of {} bytes".format(stat_)
+
+
 
 file1='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Phase1_Final_Cat_Locked.csv'
 file2='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Phase2_Final_Cat_Locked.csv'
-listOfFiles=[file1]
-constructDataset(listOfFiles)
+listOfFiles=[file1, file2]
+outputFileForDB='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Categ_For_DB.csv'
+constructDataset(listOfFiles, outputFileForDB)
