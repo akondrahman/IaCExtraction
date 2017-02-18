f36e91ef9c38674dbb5e6e99850f08440174b2f3
Akond Rahman <akond.rahman.buet@gmail.com>
2017-02-18 00:30:56 -0500

    saving for static metric

--- src/extractor/main_static_metric_extactor.py
+++ src/extractor/main_static_metric_extactor.py
@@ -3,13 +3,21 @@ Akond , Feb 17, 2017
 Placeholder for all metric extraction: static, process, chrun
 '''
 from SmellDetector import SmellDectector
+import lint_metric_extractor
+
+
+
 def getAllStaticMetric(full_path_param):
   puppet_specific_metric_for_file = SmellDectector.getQualGenratedMetricForFile(full_path_param)
   print puppet_specific_metric_for_file
+  lint_specific_metric_for_file     = lint_metric_extractor.getLintMetrics(full_path_param)
+  print lint_specific_metric_for_file
 
 
 
 
-test_hg_file=''
+
+
+test_hg_file='/Users/akond/PUPP_REPOS/mozilla-releng-downloads/relabs-puppet/manifests/site.pp'
 test_git_file='/Users/akond/PUPP_REPOS/wikimedia-downloads/cdh4/manifests/pig.pp'
-getAllStaticMetric(test_git_file)
+getAllStaticMetric(test_hg_file)
