1c455b327f4bee2cf3ea18ec3ab835ce927d7188
Akond Rahman <akond.rahman.buet@gmail.com>
2017-02-18 12:41:37 -0500

    saving for static metric

--- src/extractor/git_churn_extractor.py
+++ src/extractor/git_churn_extractor.py
@@ -2,9 +2,34 @@
 Feb 18, 2017
 Churn of files belonging to git repos
 '''
-import os, subprocess
+import os, subprocess, numpy as np
 def getRelativeChurnMetrics(param_file_path, repo_path):
+  churn_str_for_file= ""
+
   churn_added_lines = getAddedChurnMetrics(param_file_path, repo_path)
+  churn_delet_lines = getDeletedChurnMetrics(param_file_path, repo_path)
+  churn_total_lines = churn_added_lines + churn_delet_lines
+  print "Churn:add={}, churn:del={}, churn:total={}".format(churn_added_lines, churn_delet_lines, churn_total_lines)
+  lines_for_file    = sum(1 for line in open(param_file_path))
+  churn_total_days  = getDaysOfChurn(param_file_path, repo_path)
+
+  rel_churn_1 = float(churn_total_lines) / float(lines_for_file)
+  rel_churn_1 = round(rel_churn_1, 5)
+
+  rel_churn_2 = float(churn_delet_lines) / float(lines_for_file)
+  rel_churn_2 = round(rel_churn_2, 5)
+
+  rel_churn_3 = float(churn_total_lines) / float(churn_delet_lines)
+  rel_churn_3 = round(rel_churn_3, 5)
+
+  rel_churn_4 = float(churn_total_days) / float(lines_for_file)
+  rel_churn_4 = round(rel_churn_4, 5)
+
+  churn_str_for_file = str(churn_total_lines) + "," + str(rel_churn_1) + "," + str(rel_churn_2) + "," + str(rel_churn_3) + "," + str(rel_churn_4) + ","
+
+  return churn_str_for_file
+
+
 
 
 def getAddedChurnMetrics(param_file_path, repo_path):
@@ -19,8 +44,47 @@ def getAddedChurnMetrics(param_file_path, repo_path):
    add_churn_output = subprocess.check_output(['bash','-c', command2Run])
    add_churn_output = add_churn_output.split('\n')
    add_churn_output = [x_ for x_ in add_churn_output if x_!='']
-   add_churn_output = [int(y_) for y_ in add_churn_output if y_!='']
-   print add_churn_output
+   add_churn_output = [int(y_) for y_ in add_churn_output ]
+   #print add_churn_output
    totalAddedLinesForChurn = sum(add_churn_output)
-   print totalAddedLinesForChurn
+   #print totalAddedLinesForChurn
    return totalAddedLinesForChurn
+
+
+
+def getDeletedChurnMetrics(param_file_path, repo_path):
+   totalDeletedLinesForChurn = 0
+
+   cdCommand         = "cd " + repo_path + " ; "
+   theFile           = os.path.relpath(param_file_path, repo_path)
+   #print "full path: {}, repo path:{}, theFile:{}".format(param_file_path, repo_path, theFile)
+   churnDeletedCommand = "git log --numstat --oneline " + theFile + " |  awk '!(NR%2)' | awk '{ print $2 }' | sed -e  's/ /,/g'"
+   command2Run = cdCommand + churnDeletedCommand
+
+   del_churn_output = subprocess.check_output(['bash','-c', command2Run])
+   del_churn_output = del_churn_output.split('\n')
+   del_churn_output = [x_ for x_ in del_churn_output if x_!='']
+   del_churn_output = [int(y_) for y_ in del_churn_output]
+   #print del_churn_output
+   totalDeletedLinesForChurn = sum(del_churn_output)
+   #print totalDeletedLinesForChurn
+   return totalDeletedLinesForChurn
+
+
+
+def getDaysOfChurn(param_file_path, repo_path):
+   totalDaysForChurn = 0
+
+   cdCommand            = "cd " + repo_path + " ; "
+   theFile              = os.path.relpath(param_file_path, repo_path)
+   churnDateTimeCommand = "git log  --format=%cd " + theFile + " | awk '{ print $2 $3 $5}' | sed -e 's/ /,/g'"
+   command2Run          = cdCommand + churnDateTimeCommand
+
+   dt_churn_output = subprocess.check_output(['bash','-c', command2Run])
+   dt_churn_output = dt_churn_output.split('\n')
+   dt_churn_output = [x_ for x_ in dt_churn_output if x_!='']
+   dt_churn_output = np.unique(dt_churn_output)
+   #print dt_churn_output
+   totalDaysForChurn = len(dt_churn_output)
+   #print totalDaysForChurn
+   return totalDaysForChurn
