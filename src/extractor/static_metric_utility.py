309c7f28bd9684f78a61ee9109d5f476d5ccbf62
Akond Rahman <akond.rahman.buet@gmail.com>
2017-02-19 20:31:05 -0500

    saving for static metric

--- src/extractor/static_metric_utility.py
+++ src/extractor/static_metric_utility.py
@@ -40,13 +40,19 @@ def dumpContentIntoFile(strP, fileP):
 
 
 def createDataset(str2Dump, datasetNameParam):
-   headerOfFile0='org,'
-   headerOfFile1='pkg_usg,url_usg,file_usg,location_usg,SLOC,location_per_sloc,incl_usg,req_usg,ens_usg,unless_usg,before_usg,'
-   headerOfFile2='dependency,dependency_per_sloc,define_usg,reff_usg,cond_usg,namenode_usg,cron_usg,param_usg,hard_code,'
-   headerOfFile3='hard_code_per_sloc,comment_cnt,comm_cnt_per_SLOC,run_int,command_cnt,path_cnt,ssh_auth_cnt,file_mode_cnt,'
-   headerOfFile4='role_cnt,secu_cnt,secu_cnt_per_SLOC,svc_cnt,nameserver_cnt,ip_cnt,virt_cnt,net_cnt,net_cnt_per_SLOC,'
-   headerOfFile5='LINT_ERR_CNT,LINT_ERR_RATE,LINT_WARN_CNT,LINT_WARN_RATE,'
-   headerOfFile6='tot_churn_SLOC,churn_per_SLOC,churn_del_per_SLOC,tot_churn_per_del_churn,churnday_per_SLOC,tot_churn_cnt,'
+   headerOfFile0='org,file_,'
+   #headerOfFile1='pkg_usg,url_usg,file_usg,location_usg,SLOC,location_per_sloc,incl_usg,req_usg,ens_usg,unless_usg,before_usg,'
+   headerOfFile1='url_usg,file_usg,location_usg,SLOC,req_usg,ens_usg,'
+   #headerOfFile2='dependency,dependency_per_sloc,define_usg,reff_usg,cond_usg,namenode_usg,cron_usg,param_usg,hard_code,'
+   headerOfFile2='dependency,reff_usg,hard_code,'
+   #headerOfFile3='hard_code_per_sloc,comment_cnt,comm_cnt_per_SLOC,run_int,command_cnt,path_cnt,ssh_auth_cnt,file_mode_cnt,'
+   headerOfFile3='hard_code_per_sloc,comment_cnt,command_cnt,file_mode_cnt,'
+   #headerOfFile4='role_cnt,secu_cnt,secu_cnt_per_SLOC,svc_cnt,nameserver_cnt,ip_cnt,virt_cnt,net_cnt,net_cnt_per_SLOC,'
+   headerOfFile4='secu_cnt,secu_cnt_per_SLOC,'   
+   #headerOfFile5='LINT_ERR_CNT,LINT_ERR_RATE,LINT_WARN_CNT,LINT_WARN_RATE,'
+   headerOfFile5='LINT_WARN_RATE,'
+   #headerOfFile6='tot_churn_SLOC,churn_per_SLOC,churn_del_per_SLOC,tot_churn_per_del_churn,churnday_per_SLOC,tot_churn_cnt,'
+   headerOfFile6='tot_churn_SLOC,churn_per_SLOC,churn_del_per_SLOC,churnday_per_SLOC,tot_churn_cnt,'
    headerOfFile7='defect_status'
 
    headerStr = headerOfFile0 + headerOfFile1 + headerOfFile2 + headerOfFile3 + headerOfFile4 + headerOfFile5 + headerOfFile6 + headerOfFile7
