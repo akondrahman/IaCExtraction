71d6dc1702f45b380a49c6f929e3440d8ce67745
Akond Rahman <akond.rahman.buet@gmail.com>
2017-02-28 23:40:32 -0500

    cross org ?

--- src/modeling/cross_org_main.py
+++ src/modeling/cross_org_main.py
@@ -3,7 +3,7 @@ cross organization
 defect prediction
 '''
 from sklearn import decomposition
-import Utility , numpy as np , sklearn_models, pandas as pd
+import Utility , numpy as np , cross_org_sklearn_models, pandas as pd
 glimpseIndex = 10
 top_compo    = 5
 pcasToDo     = 10
@@ -16,11 +16,10 @@ print "Started at:", Utility.giveTimeStamp()
 '''
 Deprecating warnings will be suppressed
 '''
-dataset_file="/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_MOZ_FULL_DATASET.csv"
-#dataset_file="/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_WIKI_FULL_DATASET.csv"
-print "The dataset is:", dataset_file
-print "-"*50
-full_dataset_from_csv = Utility.getDatasetFromCSV(dataset_file)
+mozilla_file   = "/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_MOZ_FULL_DATASET.csv"
+wikimedia_file = "/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_WIKI_FULL_DATASET.csv"
+
+full_dataset_from_csv = Utility.getDatasetFromCSV(mozilla_file)
 full_rows, full_cols = np.shape(full_dataset_from_csv)
 print "Total number of columns", full_cols
 ## we will skip the first column, as it has file names
@@ -28,10 +27,10 @@ feature_cols = full_cols - 1  ## the last column is defect status, so one column
 all_features = full_dataset_from_csv[:, 2:feature_cols]
 print "Glimpse at features (11th entry in dataset): \n", all_features[glimpseIndex]
 print "-"*50
-dataset_for_labels = Utility.getDatasetFromCSV(dataset_file)  ## unlike phase-1, the labels are '1' and '0', so need to take input as str
+wikimedia_dataset = Utility.getDatasetFromCSV(wikimedia_file)  ## unlike phase-1, the labels are '1' and '0', so need to take input as str
 label_cols = full_cols - 1
-all_labels  =  dataset_for_labels[:, label_cols]
-print "Glimpse at  labels (11th entry in dataset):", all_labels[glimpseIndex]
+wikimedia_labels  =  wikimedia_dataset[:, label_cols]
+print "Glimpse at mozilla labels (11th entry in dataset):", wikimedia_labels[glimpseIndex]
 print "-"*50
 pcaObj = decomposition.PCA(n_components=pcasToDo)
 pcaObj.fit(all_features)
@@ -49,4 +48,9 @@ selected_features = pcaObj.fit_transform(all_features)
 print "Selected feature dataset size:", np.shape(selected_features)
 print "-"*50
 getPCAInsights(pcaObj, 1)
+print "-"*50
+#cross_org_sklearn_models.performModeling(selected_features, wikimedia_labels)
+# sklearn_models.performModeling(selected_features, all_labels, 10)
+# print "-"*50
+print "The trainer:{} \n trainee:{}".format(mozilla_file, wikimedia_file)
 print "-"*50
