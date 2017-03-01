210173d7c00859492fa52ccfec215d35afe2b2b2
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-01 00:06:52 -0500

    cross org ?

--- src/modeling/cross_org_main.py
+++ src/modeling/cross_org_main.py
@@ -19,18 +19,22 @@ Deprecating warnings will be suppressed
 mozilla_file   = "/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_MOZ_FULL_DATASET.csv"
 wikimedia_file = "/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_WIKI_FULL_DATASET.csv"
 
-full_dataset_from_csv = Utility.getDatasetFromCSV(mozilla_file)
+full_dataset_from_csv = Utility.getDatasetFromCSV(wikimedia_file)
 full_rows, full_cols = np.shape(full_dataset_from_csv)
 print "Total number of columns", full_cols
 ## we will skip the first column, as it has file names
 feature_cols = full_cols - 1  ## the last column is defect status, so one column to skip
 all_features = full_dataset_from_csv[:, 2:feature_cols]
+# all_features = all_features[0:296, :]   ## the test dataset: wikimedi adataset has 296 entries , if you ned the 296th, state 297
 print "Glimpse at features (11th entry in dataset): \n", all_features[glimpseIndex]
 print "-"*50
-wikimedia_dataset = Utility.getDatasetFromCSV(wikimedia_file)  ## unlike phase-1, the labels are '1' and '0', so need to take input as str
+wikimedia_dataset = Utility.getDatasetFromCSV(wikimedia_file)  ##
+mozilla_dataset   = Utility.getDatasetFromCSV(mozilla_file)  ##
 label_cols = full_cols - 1
-wikimedia_labels  =  wikimedia_dataset[:, label_cols]
-print "Glimpse at mozilla labels (11th entry in dataset):", wikimedia_labels[glimpseIndex]
+# wikimedia_labels  =  wikimedia_dataset[:, label_cols]
+mozilla_labels  =  mozilla_dataset[:, label_cols]
+mozilla_labels  =  mozilla_labels[0:296]
+print "Glimpse at mozilla labels (11th entry in dataset):", mozilla_labels[glimpseIndex]
 print "-"*50
 pcaObj = decomposition.PCA(n_components=pcasToDo)
 pcaObj.fit(all_features)
@@ -49,8 +53,8 @@ print "Selected feature dataset size:", np.shape(selected_features)
 print "-"*50
 getPCAInsights(pcaObj, 1)
 print "-"*50
-#cross_org_sklearn_models.performModeling(selected_features, wikimedia_labels)
-# sklearn_models.performModeling(selected_features, all_labels, 10)
-# print "-"*50
-print "The trainer:{} \n trainee:{}".format(mozilla_file, wikimedia_file)
+# cross_org_sklearn_models.performModeling(selected_features, wikimedia_labels, 10)
+cross_org_sklearn_models.performModeling(selected_features, mozilla_labels, 10)
+print "-"*50
+print "The trainer:{} \n trainee:{}".format(wikimedia_file, mozilla_file)
 print "-"*50
