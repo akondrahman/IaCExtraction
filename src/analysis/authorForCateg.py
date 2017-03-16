6a3a1a2ec8c85b8d30bc38c217590b72e99a0ac2
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-15 20:51:23 -0400

    code for authroship

--- src/analysis/authorForCateg.py
+++ src/analysis/authorForCateg.py
@@ -7,7 +7,7 @@ import csv, os
 
 
 
-def getAuthorContent(dirParam, file2check):
+def getAuthorContent(dirParam):
     authorDict={}
     no_file   =dirParam + 'randFile_n_ninety_metrics.csv'
     yes_file  =dirParam + 'randFile_y_ninety_metrics.csv'
@@ -15,14 +15,37 @@ def getAuthorContent(dirParam, file2check):
       reader_ = csv.reader(yes_f)
       for row in reader_:
           fileName  =row[0]
-          if(file2check==fileName):
-             authorCnt =row[88]
-             authorDict[fileName] = authorCnt
+          authorCnt =row[88]
+          authorDict[fileName] = authorCnt
     with open(no_file, 'rU') as no_f:
       reader_ = csv.reader(no_f)
       for row in reader_:
           fileName  =row[0]
-          if(file2check==fileName):
-             authorCnt =row[88]
-             authorDict[fileName] = authorCnt
+          authorCnt =row[88]
+          authorDict[fileName] = authorCnt
+    print authorDict
     return authorDict
+
+
+def dumpContentIntoFile(strP, fileP):
+  fileToWrite = open( fileP, 'w');
+  fileToWrite.write(strP );
+  fileToWrite.close()
+  return str(os.stat(fileP).st_size)
+
+
+def processFilesForAuthorCount(fileParam):
+    strToCreate=''
+    with open(fileParam, 'rU') as file_:
+      reader_ = csv.reader(file_)
+      for row in reader_:
+          dirName   =row[1]
+          fileName  =row[4]
+          authorCount = getAuthorContent(dirName)
+          strToCreate = strToCreate + fileName + ',' + authorCount + ',' + '\n'
+    #output = dumpContentIntoFile(strToCreate, '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/filesWithAuthorCount.csv')
+    #print "Dumped a file of {} bytes, with authro information.".format(output)
+
+
+theFIle='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/New.Categ.csv'
+processFilesForAuthorCount(theFIle)
