0582ea525518f33986f4ae381be3d58045f78f2b
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-17 14:12:54 -0400

    analysis of ODC types

--- src/analysis/authorForCateg.py
+++ src/analysis/authorForCateg.py
@@ -13,18 +13,23 @@ def getAuthorContent(dirParam):
     yes_file  =dirParam + 'randFile_y_ninety_metrics.csv'
     with open(yes_file, 'rU') as yes_f:
       reader_ = csv.reader(yes_f)
+      next(reader_, None)
       for row in reader_:
           fileName  =row[0]
           authorCnt =row[88]
           authorDict[fileName] = authorCnt
     with open(no_file, 'rU') as no_f:
       reader_ = csv.reader(no_f)
+      next(reader_, None)
       for row in reader_:
           fileName  =row[0]
           authorCnt =row[88]
           authorDict[fileName] = authorCnt
-    print authorDict
+    #print authorDict
     return authorDict
+
+
+
 
 
 def dumpContentIntoFile(strP, fileP):
@@ -34,17 +39,27 @@ def dumpContentIntoFile(strP, fileP):
   return str(os.stat(fileP).st_size)
 
 
+
+
+
 def processFilesForAuthorCount(fileParam):
     strToCreate=''
+    counter = 0
     with open(fileParam, 'rU') as file_:
       reader_ = csv.reader(file_)
+      next(reader_, None)
       for row in reader_:
+          counter += 1
+          #print "processing file #", counter
           dirName   =row[1]
+          if dirName[-1] != '/':
+            dirName = dirName + '/'
           fileName  =row[4]
-          authorCount = getAuthorContent(dirName)
+          authorCount = getAuthorContent(dirName)[fileName]
           strToCreate = strToCreate + fileName + ',' + authorCount + ',' + '\n'
-    #output = dumpContentIntoFile(strToCreate, '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/filesWithAuthorCount.csv')
-    #print "Dumped a file of {} bytes, with authro information.".format(output)
+    output = dumpContentIntoFile(strToCreate, '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/filesWithAuthorCount.csv')
+    print "Dumped a file of {} bytes, with authro information.".format(output)
+
 
 
 theFIle='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/New.Categ.csv'
---
+++ src/analysis/defect_categ_per_file.py
@@ -0,0 +1,68 @@
+'''
+are some files have
+more types of defects than
+others
+March 17, 2017
+Akond Rahman
+'''
+import os, csv, numpy as np
+
+def getFileToDefectCateg(fileParam, orgName):
+   dict2Ret={}
+   with open(fileParam, 'rU') as f:
+     reader_ = csv.reader(f)
+     for row in reader_:
+        fileName = row[4]
+        categ    = row[3]
+        if orgName in fileName:
+           if fileName not in dict2Ret:
+              dict2Ret[fileName] = [categ]
+           else:
+              dict2Ret[fileName] =  dict2Ret[fileName] + [categ]
+   return dict2Ret
+
+
+def processFileToCategMapping(dictP):
+
+    theZeroDefectList      = []
+    atLeastOneDefectList   = []
+    atLeastTwoDefectList   = []
+    atLeastThreeDefectList = []
+    atLeastFourDefectList  = []
+    atLeastFiveDefectList  = []
+    atLeastSixDefectList   = []
+    atLeastSevenDefectList = []
+    atLeastEightDefectList = []
+    atLeastNineDefectList  = []
+    for k_, v_ in dictP.items():
+        unique_defect_categ = np.unique(v_)
+        unique_defect_categ = [defect_type for defect_type in unique_defect_categ if defect_type !='' ]
+        if((len(unique_defect_categ)==1) and (unique_defect_categ[0]=='N')):
+            theZeroDefectList.append(k_)
+        elif((len(unique_defect_categ)==1) and (unique_defect_categ[0]!='N')):
+            atLeastOneDefectList.append(k_)
+        elif((len(unique_defect_categ)==2) ):
+            atLeastTwoDefectList.append(k_)
+        elif((len(unique_defect_categ)==3) ):
+            atLeastThreeDefectList.append(k_)
+        elif((len(unique_defect_categ)==4) ):
+            atLeastFourDefectList.append(k_)
+        elif((len(unique_defect_categ)==5) ):
+            atLeastFiveDefectList.append(k_)
+        elif((len(unique_defect_categ)==6) ):
+            atLeastSixDefectList.append(k_)
+        elif((len(unique_defect_categ)==7) ):
+            atLeastSevenDefectList.append(k_)
+        elif((len(unique_defect_categ)==8) ):
+            atLeastEightDefectList.append(k_)
+        elif((len(unique_defect_categ)==9) ):
+            atLeastNineDefectList.append(k_)
+    print "Count of files with no defects:", len(theZeroDefectList)
+
+theFile='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/New.Categ.csv'
+moziDict = getFileToDefectCateg(theFile, 'mozilla')
+wikiDict = getFileToDefectCateg(theFile, 'wikimedia')
+# print moziDict
+# print "="*100
+# print wikiDict
+processFileToCategMapping(moziDict)
